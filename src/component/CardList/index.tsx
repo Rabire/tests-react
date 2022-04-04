9b39c1a2747db57110fdb60eda6ff5c5022a1337
Rabire <rabireh@outlook.fr>
2022-04-04 19:59:08 +0200

    done !

--- src/component/CardList/index.tsx
+++ src/component/CardList/index.tsx
@@ -3,25 +3,16 @@ import { Card, CardType } from 'component/Card';
 import { CardsList } from './styles';
 
 export function CardList() {
-  const {
-    isLoading,
-    error,
-    data: cards
-  } = useQuery('cardsData', () => fetch('https://picsum.photos/v2/list').then((res) => res.json()));
+  const { isLoading, data: cards } = useQuery('cardsData', () =>
+    fetch('https://picsum.photos/v2/list').then((res) => res.json())
+  );
 
   return (
     <CardsList>
-      {Array(5)
-        .fill(true)
-        .map((_, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Card key={i} isLoading />
-        ))}
-
-      {/* {isLoading
+      {isLoading
         ? // eslint-disable-next-line react/no-array-index-key
           Array(5).map((_, i) => <Card key={i} isLoading />)
-        : cards.map((card: CardType) => <Card key={card.id} card={card} isLoading={false} />)} */}
+        : cards.map((card: CardType) => <Card key={card.id} card={card} isLoading={false} />)}
     </CardsList>
   );
 }
