b4a1446757de19b52f4ab5cd0374d718055c45a6
Rabire <rabireh@outlook.fr>
2022-04-05 02:46:05 +0200

    rem

--- src/component/CardList/styles.ts
+++ src/component/CardList/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const CardsList = styled.div`
   background: pink;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
   grid-gap: 1.5rem;
   margin: 3rem auto;
 `;
