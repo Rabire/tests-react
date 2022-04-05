011fc10915f35d23b8006b2ae8ace920b70088c6
Rabire <rabireh@outlook.fr>
2022-04-05 02:42:17 +0200

    padding to responsive grid

--- src/component/Sections/styles.ts
+++ src/component/Sections/styles.ts
@@ -5,8 +5,9 @@ export const Container = styled.section`
   display: grid;
   gap: 1rem;
   grid-template-columns: repeat(auto-fill, minmax(50rem, 1fr));
-  max-width: 101rem; // 50*2 + 1
   margin: 0 auto;
+  padding: 0 2rem;
+  max-width: 105rem; // 50*2 + 1(gap) + 2*2(padding)
 `;
 
 export const SectionGroup = styled.div`
