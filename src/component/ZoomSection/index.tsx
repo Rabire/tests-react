2ab86a7d74f216223f053ce69cf313a3ea32b4c8
Rabire <rabireh@outlook.fr>
2022-04-05 23:40:52 +0200

    handleMove ok

--- src/component/ZoomSection/index.tsx
+++ src/component/ZoomSection/index.tsx
@@ -6,9 +6,8 @@ export function ZoomSection() {
   const wrapper = useRef<HTMLDivElement>(null);
 
   const [zoom, setZoom] = useState(1);
+  const [position, setPosition] = useState<[number, number]>([0, 0]);
   const [isClicking, setIsClicking] = useState(false);
-
-  console.log({ isClicking });
 
   window.onscroll = () => window.scrollTo(0, 0); // DISABLE SCROLL
 
@@ -21,25 +20,35 @@ export function ZoomSection() {
       return setZoom((prev) => prev + 0.1);
     };
 
-    if (wrapper.current) {
-      wrapper.current.addEventListener('mousedown', handleClickStart); // HANDLE CLICK
-      wrapper.current.addEventListener('mouseup', handleClickEnd); // HANDLE CLICK
+    const handleMove = (e: MouseEvent) => {
+      if (!isClicking) return;
 
-      wrapper.current.addEventListener('wheel', handleZoom); // HANDLE ZOOM
-    }
+      setPosition((prev) => {
+        const newX = prev[0] + e.movementX / 5;
+        const newY = prev[1] + e.movementY / 5;
+        return [newX, newY];
+      });
+    };
+
+    wrapper?.current?.addEventListener('mousedown', handleClickStart); // HANDLE CLICK
+    wrapper?.current?.addEventListener('mouseup', handleClickEnd); // HANDLE CLICK
+    wrapper?.current?.addEventListener('mousemove', handleMove); // HANDLE MOVE
+    wrapper?.current?.addEventListener('wheel', handleZoom); // HANDLE ZOOM
 
     return () => {
       wrapper?.current?.removeEventListener('mousedown', handleClickStart); // HANDLE CLICK
       wrapper?.current?.removeEventListener('mouseup', handleClickEnd); // HANDLE CLICK
-
-      document.removeEventListener('wheel', handleZoom); // HANDLE ZOOM
+      wrapper?.current?.removeEventListener('mousemove', handleMove); // HANDLE MOVE
+      wrapper?.current?.removeEventListener('wheel', handleZoom); // HANDLE ZOOM
     };
-  }, []);
+  }, [isClicking]);
 
   return (
     <ComponentBox>
       <MapWrapper ref={wrapper}>
-        <France style={{ transform: `scale(${zoom})` }} />
+        <France
+          style={{ transform: `scale(${zoom}) translate(${position[0]}px, ${position[1]}px)` }}
+        />
       </MapWrapper>
     </ComponentBox>
   );
